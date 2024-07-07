dbc903bbe7652d51c4f3cfa3b0bc2fccf29d0655
Mathieu Lacroix <mathieu.lacroix@universite-paris-saclay.fr>
2024-07-07 18:46:03 +0200

    dddd

--- backend/src/user/user.model.ts
+++ backend/src/user/user.model.ts
@@ -18,7 +18,7 @@ export class User {
     @Field(type => Int)
     timeStamp: number;
 
-    @Field(type => [ID])
+    @Field(type => [Conversation], { nullable: true })
     conversationsIds?: Conversation[] | null;
 
 }
